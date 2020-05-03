ba4c9d25c41b983d1d41ae75ba284e0d3a478037
Artem <triple8six@gmail.com>
2020-05-03 17:32:09 -0400

    p

--- src/BinaryTrees/numOfHalfNodes.java
+++ src/BinaryTrees/numOfHalfNodes.java
@@ -15,6 +15,20 @@ public class numOfHalfNodes {
 	    int hcount = 0;
 	    while(!que.isEmpty()) {
 	        int count = que.size();
+	        //we do this to control the levels
+	        //LIKE THIS https://stackoverflow.com/questions/1894846/printing-bfs-binary-tree-in-level-order-with-specific-formatting
+			//How would you modify your program to print it so,
+			//
+			//	        1
+			//	        2 3
+			//	        4 5 6
+			//Rather than the general
+			//	        1 
+			//	        2 
+			//	        3 
+			//	        4 
+			//	        5 
+//	        6
 	        for(int i = 0;i<count;i++) {//dont incude 0, as we already put it, so <, this makes sure a level finishes, level 2^0,2^1,2^2 
 	            curr = que.poll();
 	            
