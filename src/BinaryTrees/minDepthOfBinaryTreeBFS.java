fdd3143ac4108fa89a3da33d05de4c10056fe053
Artem <triple8six@gmail.com>
2020-05-02 20:47:23 -0400

    p

--- src/BinaryTrees/minDepthOfBinaryTreeBFS.java
+++ src/BinaryTrees/minDepthOfBinaryTreeBFS.java
@@ -6,6 +6,32 @@ import java.util.Queue;
 
 
 public class minDepthOfBinaryTreeBFS {
+	public int minTreeDepth2(TreeNode root) {
+	    //bfslevel
+	    if(root == null) return 0;
+	    TreeNode curr = root;
+	    Queue<TreeNode> que = new LinkedList<TreeNode>();
+	    que.offer(root);
+	    int count = 1;
+	    int cnt = 0;
+	    while(que.size() > 0) {
+	        cnt = que.size();
+	        for(int i = 0;i<=cnt;i++) { //inital 1, break ++, 2nd iter,  2,3
+	            if(curr.left == null && curr.right == null) {
+	                return count;
+	            }
+	            if(curr.left != null) {
+	                que.offer(curr.left);
+	            }
+	            if(curr.right != null) {
+	                que.offer(curr.right);
+	            }
+	            curr = que.poll();  
+	        }
+	        count++;
+	    }
+	    return 0;
+	}
 	public int minDepth(TreeNode root) {
 	    if(root == null) return 0;
 	    int depth = 1;
