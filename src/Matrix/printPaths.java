ed7c23f9837cd5a22fc03cb676abf19fbe935b18
Artem <triple8six@gmail.com>
2020-05-03 23:38:36 -0400

    p

--- src/Matrix/printPaths.java
+++ src/Matrix/printPaths.java
@@ -6,32 +6,31 @@ public class printPaths {
 	public ArrayList<String> printPaths(char[][] board){
 	    ArrayList<String> ar = new ArrayList<String>();
 	    String s = "";
-	    int row = board.length;
-	    int col = board[0].length;
+	    int row = board.length-1;
+	    int col = board[0].length-1;
 	    
-	    for(int i = 0;i<row;i++) {
-	        for(int j = 0;j<col;j++) {
+	    for(int i = 0;i<=row;i++) {
+	        for(int j = 0;j<=col;j++) {
 	            //dfs, and sink
-	            ar.add(dfs(board, i, j, s));
+	            dfs(board, i, j, s);
+	            if(board[i][j] == board[row][col]) {
+	               ar.add(s);
+	               s = "";
+	            } 
+	            
 	        }
 	    }
 	    return ar;
 	}
 
-	private String dfs(char[][] m, int i, int j, String s) {
-	    if(i < 0 || j < 0 || i > m[0].length || j > m.length) return s;
-
-	    char temp = m[i][i];
+	private void dfs(char[][] m, int i, int j, String s) {
+	    if(i < 0 || j < 0 || j > m[0].length-1 || i > m.length-1) return;
+	    s = s+(m[i][i]);
 	    m[i][i] ^= 256;
-	    
 	    dfs(m, i+1, j, s);//down on row
 	    dfs(m, i, j+1, s);//right on col
-	    if(temp == m[m.length-1][m[0].length-1]) {
-	        s = s+(temp);
-	    }
-	    
+
 	    m[i][i] ^= 256;
-		return s;
 	}
 
 
