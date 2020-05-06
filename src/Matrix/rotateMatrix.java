b5c8b813f35eb54148c20342e72b58a9879800f8
Artem <triple8six@gmail.com>
2020-05-06 01:40:26 -0400

    p

--- src/Matrix/rotateMatrix.java
+++ src/Matrix/rotateMatrix.java
@@ -3,13 +3,13 @@ package Matrix;
 public class rotateMatrix {
 
 		public void rotate(int[][] matrix) {
-		    int n=matrix.length;
-		    for (int i=0; i<n/2; i++) 
-		        for (int j=i; j<n-i-1; j++) {
+		    int n=matrix.length-1;
+		    for (int i=0; i<=n/2; i++) 
+		        for (int j=i; j<=n-i; j++) {
 		            int tmp=matrix[i][j];
-		            matrix[i][j]=matrix[n-j-1][i];
-		            matrix[n-j-1][i]=matrix[n-i-1][n-j-1];
-		            matrix[n-i-1][n-j-1]=matrix[j][n-i-1];
+		            matrix[i][j]=matrix[n-j][i];
+		            matrix[n-j-1][i]=matrix[n-i-1][n-j];
+		            matrix[n-i-1][n-j-1]=matrix[j][n-i];
 		            matrix[j][n-i-1]=tmp;
 		        }
 		}
