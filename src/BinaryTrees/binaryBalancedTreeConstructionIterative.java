0aa3be4cfee3a2cd0c6cae06adac6fec4e402bb0
Artem <triple8six@gmail.com>
2020-05-27 11:26:19 -0400

    p

--- src/BinaryTrees/binaryBalancedTreeConstructionIterative.java
+++ src/BinaryTrees/binaryBalancedTreeConstructionIterative.java
@@ -1,10 +1,58 @@
 package BinaryTrees;
 
+import java.util.Stack;
+
 public class binaryBalancedTreeConstructionIterative {
+	//THIS WORKS ONLY IF THE TREE IS A BST!!!!!!!!!!!!!!! you spent a lot of time thiniking your wrong, make sure to understand exatly
+	//and litererally what the problem is asking. 
+    public static String searchBST(TreeNode root, int target) {
+        //TreeNode curr = root;
+        if(root == null) return "";
+        if(root.val == target) return "";
+        Stack<TreeNode> stk = new Stack<TreeNode>();
+        
+        if(root.val > target) {
+            //curr = curr.left;
+            stk.push(root.left);
+        } else {
+            //curr = curr.right;
+            stk.push(root.right);
+        } 
+        
+        while(!stk.isEmpty()) {
+            TreeNode curr = stk.pop();
 
+            if(curr != null) {
+                if(curr.val == target) {
+                    return "found";
+                } 
+
+                if(curr.val > target) {
+                    //curr = curr.left;
+                    stk.push(curr.left);
+                } 
+
+                if(curr.val < target)
+                    //curr = curr.right;
+                    stk.push(curr.right);
+                } 
+
+
+            }
+            //for the next iteration
+        return "not found";
+        
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			//BALANCED!
+		   TreeNode tree = new TreeNode(4);
+	       tree.left = new TreeNode(2); 
+	       tree.right = new TreeNode(7); 
+	       tree.left.left = new TreeNode(1); 
+	       tree.left.right = new TreeNode(3); 
+	       tree.right.left = new TreeNode(6); //for seriralize binary tree
+	       tree.right.right = new TreeNode(10);//the oppiste of a postorder print gives us an ablity to build via preprder-IE prePLR, postLRP
+	       System.out.println(searchBST(tree, 6));//prints 1,2,6,4,2,4,3 -- So if we 
 	}
 
 }
--- src/BinaryTrees/binaryFindIterative.java
+++ src/BinaryTrees/binaryFindIterative.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 public class binaryFindIterative {//ie level order
 	public static boolean findNode(TreeNode root, int val) {
-	    
+	    //IF IT"S NOT A BST, OTHERWISE WE CAN CONTROL IF WE NEED TO GO LEFT OR RIGHT!!!!*****
 	    //iteration with trees requres a stack for order traversals, and que for level order
 	    //thus there's two ways to do this.
 	    Queue<TreeNode> q = new LinkedList<TreeNode>();
@@ -34,8 +34,14 @@ public class binaryFindIterative {//ie level order
 		return false;
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		   TreeNode tree = new TreeNode(1);
+	       tree.left = new TreeNode(2); 
+	       tree.right = new TreeNode(2); 
+	       tree.left.left = new TreeNode(6); 
+	       tree.left.right = new TreeNode(4); 
+	       tree.right.left = new TreeNode(4); //for seriralize binary tree
+	       tree.right.right = new TreeNode(3);//the oppiste of a postorder print gives us an ablity to build via preprder-IE prePLR, postLRP
+	       //System.out.println(postOrder(tree));//prints 1,2,6,4,2,4,3 -- So if we 
 	}
 }	
 //		Given a binary tree, write a method to find and return the node with data = the input data. Do not use recursion.
