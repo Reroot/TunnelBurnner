e80d49c16361f9c32813500b0efa8a6aa0b99e1a
tripl <triple8six@gmail.com>
2020-06-07 17:19:58 -0400

    p

--- src/GoogleFreeBalling/longestAbsPath.java
+++ src/GoogleFreeBalling/longestAbsPath.java
@@ -4,19 +4,40 @@ import java.util.HashMap;
 
 public class longestAbsPath {
     public static int lengthLongestPath2(String input) {//using run 1 as ex
+        char[] str = input.toCharArray();
+        for(char c : str) {
+        	System.out.print(c);
+        }
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.println("[file2.ext as the most 3 tabs to get to it]");
+        System.out.println("");
         String[] parts = input.split("\n");
         HashMap<Integer, Integer> mapLevel = new HashMap<Integer, Integer>();//@level, count at level - use to retore and contuine count
         //level , length
         int maxLen = 0;
         for(String part : parts) {
-            int level = part.lastIndexOf("\t") + 1;//0 ts at part 1 + 1
-            int len = part.length() - level;//3-1 = 2//iter1@len is 2
-            if(level != 0) {//skip this initally
+            int level = part.lastIndexOf("\t") + 1;//0,1,2,2,
+            System.out.print("level as current tab as depth -");
+            System.out.print(level);
+            System.out.print("-");
+            int len = part.length() - level;//subtract the tab levels, before we use add this part to our prevoius chain
+            
+            
+//reduce of len = currentPartLength - it's tab spaces, 
+            //then restore the length of all the sum from mapped prev len of all parts, and now add the add the new part to the length
+            //-1 becuase we need the prev tabbedlengthpath, we already have the length of the new part. 
+            if(level != 0) {//skip this initally, add the current level back, based on 
                 // parent would be always at level - 1
                 // add +1 to account for forward slash
-                len = mapLevel.get(level - 1) + len + 1;//get {0:3}   
+                len = mapLevel.get(level - 1) + len + 1;//get {0:3}, Get the Last mapped level to contuine off of
             }
-            mapLevel.put(level, len);// iter1@map{0:3} iter2@map
+            mapLevel.put(level, len);// iter1@map{0:3}{1:11}{2:21} iter2@map
+            System.out.print("-len-");
+            System.out.print(len);
+            System.out.print("--last index of (.) ");
+            //hit at 7 and 8
             System.out.println(part.indexOf("."));
             if(part.indexOf(".") != -1) {//runs at 6 (\t\tfile.ext), index of default to -1
                 maxLen = Math.max(maxLen, len);// will run for each file and save, 
@@ -25,69 +46,6 @@ public class longestAbsPath {
             }
         }
         return maxLen;
-    }
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	//wrong but logic is corrrect impelentation fail -- idk why it doesn't recoginze the "n" or /t"
-    public static int lengthLongestPath(String input) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();//@level, count at level - use to retore and contuine count
-        int curr = 0;
-        int max = Integer.MIN_VALUE;
-        char[] str = input.toCharArray();
-        for(char c : str) {
-        	System.out.print(c);
-        }
-    	//System.out.println(str.length);
-
-        		//.split("n");
-        int levelcount = 0;
-  
-        for(int i =0;i<str.length;i++) {
-        	
-            if(str[i] != '\t' || str[i] != ' ' || str[i] != '\n') {
-            	levelcount = 0;
-            	curr++;
-            }
-            
-            max = Math.max(max, curr);//save the current max before we reset to the \t, so we have the update just incase we run into something smaller later
-
-            if(str[i] == '\t') {
-                levelcount++;
-            } else {
-                map.put(levelcount, curr);
-                //restore
-                //reset "/t" level count
-            }    
-            
-            
-            curr = map.getOrDefault(levelcount, curr); //get the curr at the level or insert it
-
-            
-        }
-        return max;
     }
     
 	public static void main(String[] args) {
